d475cff0070aeea4d98d87cd4b37a4f15805f271
Luis <losjuegosdelcapibara@gmail.com>
2025-02-13 20:45:24 -0600

    Se finalizaron los retos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+let titulo = document.querySelector("h1")
+titulo.innerHTML = "Hora del Desafío"
 
+function Console(){
+    alert("El botón fue clicado")
+}
+
+function Prompt(){
+    let country = prompt("Ingresa una ciudad de brasil")
+    alert(`Estuve en ${country} y me acordé de ti`)
+}
+
+function Alert(){
+    alert("Yo amo JS")
+}
+
+function suma(){
+    let resultado = 0
+    let num1 = Number(prompt("Ingresa un numero"))
+    let num2= Number(prompt("Ingresa otro numero"))
+    resultado = num1 + num2
+    alert(`El resultado de la suma es: ${resultado}`)
+}
